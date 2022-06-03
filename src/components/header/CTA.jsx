855968eb6acb3aea21be1841aacf08f06d195a47
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-06-03 09:20:22 -0300

    actualizacion1

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,11 @@
-import CV from "../../assets/CV-FT-Juan Pablo Tuttolomondo.pdf";
+import CVSpanish from "../../assets/CV-FT-Spanish-Juan-Pablo-Tuttolomondo.pdf";
+import CVEnglish from "../../assets/CV-FT-English-Juan-Pablo-Tuttolomondo.pdf";
 
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={CV} download className="btn">Download CV</a>
+            <a href={CVSpanish} download className="btn">Download Spanish CV</a>
+            <a href={CVEnglish} download className="btn">Download English CV</a>
             <a href="#contact" className="btn btn-primary">Contact</a>
         </div>
     );
