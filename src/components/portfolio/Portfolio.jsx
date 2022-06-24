86c929690c334eba9852a4d081455813de638572
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-06-24 17:55:51 -0300

    update 1

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './portfolio.css';
 import Videogames from '../../assets/PI-videogames.jpg';
 import SotelinoGallery from '../../assets/SotelinoGallery.jpeg';
+import AppMovies from '../../assets/appMovies.jpg';
 
 const Portfolio = () => {
     return (
@@ -50,6 +51,28 @@ const Portfolio = () => {
                         </a>
                     </div>
                 </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={AppMovies} alt="App Movies" />
+                    </div>
+                    <h3>App Movies Benjamin Maggi Challenge</h3>
+                    <div className="portfolio__item-cta">
+                        <a 
+                        href="https://github.com/jptuttolomondo/appMovies" 
+                        className="btn" 
+                        target='_blank'>
+                            Github
+                        </a>
+                        <a 
+                        href="https://app-movies-jptuttolomondo-dbrdkm5i5-jptuttolomondo.vercel.app/home" 
+                        className="btn btn-primary"
+                        target='_blank'>
+                            Live Demo
+                        </a>
+                    </div>
+                </article>
+
+                
             </div>
         </section>
     );
