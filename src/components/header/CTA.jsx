eee08165ea94cb1b9299109c267eb0c159a4119e
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-08-26 10:46:23 -0300

    26/08-1

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CVEnglish from "../../assets/CVENGLISH082022.pdf";
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={CVSpanish} download className="btn">Download Spanish CV</a>
-            <a href={CVEnglish} download className="btn">Download English CV</a>
+            <a href={CVSpanish} download className="btn">Spanish CV</a>
+            <a href={CVEnglish} download className="btn">English CV</a>
             <a href="#contact" className="btn btn-primary">Contact</a>
         </div>
     );
